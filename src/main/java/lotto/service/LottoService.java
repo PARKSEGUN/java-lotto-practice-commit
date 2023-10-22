a0d9838846d6edf0579997c15b31c3549825e1b8
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-22 22:47:37 +0900

    feat(LottoService):priceToLottoCount 메서드 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,13 @@
+package lotto.service;
+
+import lotto.Validator.Validator;
+
+public class LottoService {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public int priceToLottoCount(int price) {
+        Validator.validateDivided(price, LOTTO_PRICE, "[ERROR] 로또 가격으로 나누어 떨어지지 않습니다.");
+        return price / LOTTO_PRICE;
+    }
+}
