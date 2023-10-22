8536eea67c81456274d563e205d97a02a32be8dc
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-23 01:32:31 +0900

    feat(LottoInputView):readPurchaseAmount 메서드 구현

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Validator.Validator;
+import lotto.util.Util;
+
 public class LottoInputView {
+    public static int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputString = Console.readLine();
+        int inputAmount = Util.StringToInteger(inputString);
+        Validator.validateNaturalNumber(inputAmount);
+        return inputAmount;
+    }
+
 }
