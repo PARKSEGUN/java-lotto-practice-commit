2bf925c7cc81237e49946cf4de11308888d2ddd5
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-23 01:32:55 +0900

    feat(LottoOutPutView):printLottosSize 메서드 구현

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -4,6 +4,10 @@ import java.util.List;
 import lotto.util.Util;
 
 public class LottoOutputView {
+    public static void printLottosSize(int lottosSize) {
+        System.out.println(lottosSize + "개를 구매했습니다.");
+    }
+
     public static void printLottoNumbers(List<Integer> lottoNumbers) {
         System.out.print("[");
         for (Integer lottoNumber : lottoNumbers) {
@@ -14,6 +18,6 @@ public class LottoOutputView {
             System.out.print(", ");
         }
         System.out.println("]");
-
     }
+
 }
