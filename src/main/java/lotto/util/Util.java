2f3cbe6a3cac0ba165d9cf8ad9eae23aa82c8557
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-22 23:41:03 +0900

    feat(Util):getLastElementByList 메서드 구현

--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,4 +1,15 @@
 package lotto.util;
 
+import java.util.List;
+
 public class Util {
+    private static final int INTEGER_ZERO = 0;
+    private static final int INTEGER_ONE = 1;
+    public static Object getLastElementByList(List<Object> objectList) {
+        if (objectList.size() > INTEGER_ZERO) {
+            return objectList.get(objectList.size()-INTEGER_ONE);
+        }
+        throw new IllegalArgumentException("리스트 크기가 0이면 마지막 원소를 가져올 수 없습니다");
+    }
+
 }
