f159c4f577bc0d05b115d47078f90070442a2c3e
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-23 16:42:29 +0900

    feat(LottoInputView):readWinningLotto 메서드 구현

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,10 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.Validator.Validator;
 import lotto.util.Util;
 
 public class LottoInputView {
+    private static final int LOTTO_SIZE = 6;
+
     public static int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String inputString = Console.readLine();
@@ -12,5 +15,13 @@ public class LottoInputView {
         Validator.validateNaturalNumber(inputAmount);
         return inputAmount;
     }
+
+    public static List<Integer> readWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningLottoByString = Console.readLine();
+        String[] splitWinningLottoByString = winningLottoByString.split(",");
+        return Util.stringArrayToIntegerList(splitWinningLottoByString);
+    }
+
 
 }
