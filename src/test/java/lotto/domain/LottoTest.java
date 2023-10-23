108a31d42f45682cc4734b970218a4531f804d21
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-24 03:48:56 +0900

    test(domain):입력한 당첨번호와 보너스번호를 비교해서 당첨내용 추출 테스트 코드

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -38,7 +39,7 @@ class LottoTest {
     @Test
     void 랜덤으로_지정한_번호에_예외는_없는지_확인한다() {
         Throwable result = catchThrowable(() -> {
-            Lotto.makeLotto();
+            LottoService.makeLotto();
         });
         assertThat(result).doesNotThrowAnyException();
     }
@@ -53,5 +54,25 @@ class LottoTest {
         Throwable result = catchThrowable(() -> lotto.validateLottoNotContainsLottoNumber(bonusNumber));
         //then
         assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력한 당첨번호와 보너스번호를 비교해서 당첨내용 추출")
+    @Test
+    void 입력한_당첨번호와_보너스번호를_비교해서_당첨내용_추출() {
+        //given
+        Lotto lotto1 = new Lotto(new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        Lotto lotto2 = new Lotto(new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 7)));
+        Lotto winningLotto = new Lotto(new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        int bonusNumber = 7;
+        LottoUser lottoUser = new LottoUser(0);
+        LottoWinning reusltLottoWinning1 = LottoWinning.FIRST;
+        LottoWinning reusltLottoWinning2 = LottoWinning.SECOND;
+        //when
+        lottoUser.addLotto(lotto1);
+        lottoUser.addLotto(lotto2);
+        List<LottoWinning> lottoWinnings = lottoUser.confirmWinningLotto(winningLotto, bonusNumber);
+        //then
+        assertThat(reusltLottoWinning1).isEqualTo(lottoWinnings.get(0));
+        assertThat(reusltLottoWinning2).isEqualTo(lottoWinnings.get(1));
     }
 }
