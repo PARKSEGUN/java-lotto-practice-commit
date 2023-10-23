5e190821015ff5605608b7e558d304051b11f6da
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-23 16:45:01 +0900

    feat:당첨 번호를 입력받기위한 기능들 구현

--- src/main/java/lotto/Validator/Validator.java
+++ src/main/java/lotto/Validator/Validator.java
@@ -1,11 +1,21 @@
 package lotto.Validator;
 
+import java.util.List;
+
 public class Validator {
     private static final int INTEGER_ZERO = 0;
 
-    public static void validateDivided(int target, int divideValue, String ExceptionMessage) {
+    public static void validateStringToInteger(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR]입력이 잘못 되었습니다.");
+        }
+    }
+
+    public static void validateDivided(int target, int divideValue) {
         if (target % divideValue != 0) {
-            throw new IllegalArgumentException(ExceptionMessage);
+            throw new IllegalArgumentException("[ERROR]숫자가 나누어떨어지지 않습니다.");
         }
     }
 
@@ -16,7 +26,41 @@ public class Validator {
         }
     }
 
-    public static void validateStringIsNumber(String inputString) {
+    public static void validateIntegerListInRange(List<Integer> integerList, int start, int end) {
+        for (Integer integer : integerList) {
+            validateNumberInRange(integer, start, end);
+        }
+    }
 
+    public static void validateStringContainsTargetString(String string, String target) {
+        if (!string.contains(target)) {
+            throw new IllegalArgumentException("[ERROR]" + target + "을 포함하지 않습니다.");
+        }
+    }
+
+    public static void validateNumberInRange(int number, int start, int end) {
+        if (number < start || number > end) {
+            throw new IllegalArgumentException("[ERROR]값이 범위 내에 있지 않습니다");
+        }
+    }
+
+    public static void validateStringArrayLength(String[] stringArray, int length) {
+        if (stringArray.length != length) {
+            throw new IllegalArgumentException("[ERRER]',' 을 기준으로 입력해주세요.");
+        }
+    }
+
+    public static void validateIntegerListLength(List<Integer> numbers, int length) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] ',' 을 기준으로 6자리 숫자가 입력되어야합니다.");
+        }
+    }
+
+    public static void validateIntegerListHasDifferentNumber(List<Integer> integers) {
+        for (Integer integer : integers) {
+            if (integers.contains(integer)) {
+                throw new IllegalArgumentException("중복된 원소를 갖을 수 없습니다.");
+            }
+        }
     }
 }
--- src/main/java/lotto/util/Util.java
+++ src/main/java/lotto/util/Util.java
@@ -1,6 +1,8 @@
 package lotto.util;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Validator.Validator;
 
 public class Util {
     private static final int INTEGER_ZERO = 0;
@@ -13,13 +15,34 @@ public class Util {
         throw new IllegalArgumentException("리스트 크기가 0이면 마지막 원소를 가져올 수 없습니다");
     }
 
-    public static int StringToInteger(String string) {
-        try {
-            Integer.parseInt(string);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력된 값은 숫자가 아닙니다.");
+    public static List<Integer> stringArrayToIntegerList(String[] stringArray) {
+        for (String s : stringArray) {
+            System.out.println(s);
         }
+        List<String> strings = stringArrayToStringList(stringArray);
+        return stringListToIntegerList(strings);
+    }
+
+    public static List<String> stringArrayToStringList(String[] stringArray) {
+        List<String> strings = new ArrayList<>();
+        for (String string : stringArray) {
+            strings.add(string);
+        }
+        return strings;
+    }
+
+    public static List<Integer> stringListToIntegerList(List<String> strings) {
+        List<Integer> integers = new ArrayList<>();
+        for (String string : strings) {
+            integers.add(Util.StringToInteger(string));
+        }
+        return integers;
+    }
+
+    public static int StringToInteger(String string) {
+        Validator.validateStringToInteger(string);
         return Integer.parseInt(string);
     }
+
 
 }
