5e25948177807b3a10f01d9ed51d6a3d876a2e6a
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-23 17:54:30 +0900

    feat(LottoService):purchaseLottos 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,27 @@
 package lotto.service;
 
 import lotto.Validator.Validator;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoUser;
 
 public class LottoService {
 
     private static final int LOTTO_PRICE = 1000;
 
     public int priceToLottosCount(int price) {
-        Validator.validateDivided(price, LOTTO_PRICE, "[ERROR] 로또 가격으로 나누어 떨어지지 않습니다.");
+        Validator.validateDivided(price, LOTTO_PRICE);
         return price / LOTTO_PRICE;
     }
 
+    public void purchaseLottos(LottoUser lottoUser, int purchaseCount) {
+        for (int i = 0; i < purchaseCount; i++) {
+            lottoUser.addLotto(Lotto.makeLotto());
+        }
+    }
+
+    public void ConfirmWinning(LottoUser lottoUser, LottoGame lottoGame) {
+
+    }
 
 }
