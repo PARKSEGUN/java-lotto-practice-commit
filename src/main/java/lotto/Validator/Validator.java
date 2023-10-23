0ace375cc00c4d3179514b19caba735b5fd85011
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-24 02:23:11 +0900

    refactor(Validator):기능 요구사항에 따른 수정

--- src/main/java/lotto/Validator/Validator.java
+++ src/main/java/lotto/Validator/Validator.java
@@ -1,28 +1,33 @@
 package lotto.Validator;
 
 import java.util.List;
+import lotto.util.Util;
 
 public class Validator {
     private static final int INTEGER_ZERO = 0;
+    private static final int INTEGER_TWO = 2;
 
     public static void validateStringToInteger(String string) {
         try {
             Integer.parseInt(string);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR]입력이 잘못 되었습니다.");
+            System.out.println("[ERROR]입력이 잘못 되었습니다.");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateDivided(int target, int divideValue) {
         if (target % divideValue != 0) {
-            throw new IllegalArgumentException("[ERROR]숫자가 나누어떨어지지 않습니다.");
+            System.out.println("[ERROR]숫자가 나누어떨어지지 않습니다.");
+            throw new IllegalArgumentException();
         }
     }
 
 
     public static void validateNaturalNumber(int number) {
-        if (number < INTEGER_ZERO) {
-            throw new IllegalArgumentException("[ERROR]입력된 값은 자연수가 아닙니다.");
+        if (number <= INTEGER_ZERO) {
+            System.out.println("[ERROR]로또를 구매할 수 없습니다.");
+            throw new IllegalArgumentException();
         }
     }
 
@@ -34,32 +39,38 @@ public class Validator {
 
     public static void validateStringContainsTargetString(String string, String target) {
         if (!string.contains(target)) {
-            throw new IllegalArgumentException("[ERROR]" + target + "을 포함하지 않습니다.");
+            System.out.println("[ERROR]" + target + "을 포함하지 않습니다.");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateNumberInRange(int number, int start, int end) {
         if (number < start || number > end) {
-            throw new IllegalArgumentException("[ERROR]값이 범위 내에 있지 않습니다");
+            System.out.println("[ERROR]값이 범위 내에 있지 않습니다");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateStringArrayLength(String[] stringArray, int length) {
         if (stringArray.length != length) {
-            throw new IllegalArgumentException("[ERRER]',' 을 기준으로 입력해주세요.");
+            System.out.println("[ERRER]',' 을 기준으로 입력해주세요.");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateIntegerListLength(List<Integer> numbers, int length) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] ',' 을 기준으로 6자리 숫자가 입력되어야합니다.");
+        if (numbers.size() != length) {
+            System.out.println("[ERROR] ',' 을 기준으로 " + length + "자리 숫자가 입력되어야합니다.");
+            throw new IllegalArgumentException();
         }
     }
 
     public static void validateIntegerListHasDifferentNumber(List<Integer> integers) {
         for (Integer integer : integers) {
-            if (integers.contains(integer)) {
-                throw new IllegalArgumentException("중복된 원소를 갖을 수 없습니다.");
+            int count = Util.countSameIntegerByIntegers(integer, integers);
+            if (count >= INTEGER_TWO) {
+                System.out.println("중복된 원소를 갖을 수 없습니다");
+                throw new IllegalArgumentException();
             }
         }
     }
